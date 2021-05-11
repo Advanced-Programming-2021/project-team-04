7299e2657cfb136d7973f76eb04d182ae2455722
Sorousherafat <sorousherafat@gmail.com>
2021-05-12 00:59:18 +0430

    First update on AI class

--- src/main/java/model/AI.java
+++ src/main/java/model/AI.java
@@ -1,17 +1,36 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class AI extends Duelist {
+
     private ArrayList<MonsterCard> findTributes;
 
-    public MonsterCard getMyStrongestMonster() {
+    public void attack() {
 
     }
-    public MonsterCard getOpponentsWeakestCard() {
 
+    public MonsterCard getStrongestMonster(ArrayList<MonsterCard> monsterCards) {
+        return monsterCards.stream()
+                .filter(m -> m.getMonsterCardModeInField().equals(MonsterCardModeInField.ATTACK_FACE_UP))
+                .filter(MonsterCard::canAttack).max(Comparator.comparing(m -> m.thisCardAttackPower)).orElse(null);
     }
-    public void summonMyMonster() {
+
+    public MonsterCard getOpponentsWeakestAttackCard(Duelist opponent) {
+        return opponent.getField().getMonsterCards().stream()
+                .filter(m -> m.getMonsterCardModeInField().equals(MonsterCardModeInField.ATTACK_FACE_UP))
+                .min(Comparator.comparing(m -> m.thisCardAttackPower)).orElse(null);
+    }
+
+    public MonsterCard getOpponentsWeakestDefenseCard(Duelist opponent) {
+        return opponent.getField().getMonsterCards().stream()
+                .filter(m -> m.getMonsterCardModeInField().equals(MonsterCardModeInField.DEFENSE_FACE_DOWN) ||
+                        m.getMonsterCardModeInField().equals(MonsterCardModeInField.DEFENSE_FACE_UP))
+                .min(Comparator.comparing(m -> m.thisCardDefensePower)).orElse(null);
+    }
+
+    public void summonMonster() {
 
     }
     public void attackDirectly() {
