1f68d67b10e84b222933ef276150318f9fe8c636
Sorousherafat <sorousherafat@gmail.com>
2021-06-12 07:23:58 +0430

    Initialize phase_1 branch

--- src/main/java/controller/DeckController.java
+++ src/main/java/controller/DeckController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DeckController {
+
     private static DeckController singleInstance = null;
 
     public static DeckController getInstance() {
